1695fd8efc524be984375435876f1885f2ce9ee5
jiangmengya <github19860128>
2021-11-30 21:53:20 +0800

    111

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,5 +3,7 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("[+] Hello, World, This is main.\n");
+        System.out.println("[+] This is for test.\n");
+        System.out.println("Later.\n");
     }
 }
