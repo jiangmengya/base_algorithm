cd70a52b14458caddca78788273286ad1ff3f148
jiangmengya <github19860128>
2021-11-30 21:35:51 +0800

    ceshi

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("[+] Hello, World.\n");
+        System.out.println("[+] Hello, World, This is main.\n");
     }
 }
