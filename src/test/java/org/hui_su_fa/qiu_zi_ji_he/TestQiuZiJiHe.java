c1b3b677475aa18bb0c6444518ee2ca45e2b3155
jiangmengya <github19860128>
2021-12-07 11:19:39 +0800

    subset done

---
+++ src/test/java/org/hui_su_fa/qiu_zi_ji_he/TestQiuZiJiHe.java
@@ -0,0 +1,11 @@
+package org.hui_su_fa.qiu_zi_ji_he;
+
+import org.junit.Test;
+
+public class TestQiuZiJiHe {
+    @Test
+    public void doTest(){
+        int[] ints = {1, 2, 4};
+        QiuZiJiHe.entrance(ints);
+    }
+}
